522e46e5fa670f709fbb71e79dcf5fc76fc1b712
Bruno Michael <bruno.facomp2016@gmail.com>
2025-05-22 22:58:47 -0300

    db: Banco atualizado para importação.

--- drogaria.sql
+++ drogaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 22/05/2025 às 16:43
+-- Tempo de geração: 23/05/2025 às 01:57
 -- Versão do servidor: 9.3.0
 -- Versão do PHP: 8.2.28
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cache` (
-  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `value` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `expiration` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -38,8 +38,8 @@ CREATE TABLE `cache` (
 --
 
 INSERT INTO `cache` (`key`, `value`, `expiration`) VALUES
-('drogaria_cache_livewire-rate-limiter:a17961fa74e9275d529f489537f179c05d50c2f3', 'i:2;', 1747926630),
-('drogaria_cache_livewire-rate-limiter:a17961fa74e9275d529f489537f179c05d50c2f3:timer', 'i:1747926630;', 1747926630);
+('drogaria_cache_livewire-rate-limiter:a17961fa74e9275d529f489537f179c05d50c2f3', 'i:1;', 1747964598),
+('drogaria_cache_livewire-rate-limiter:a17961fa74e9275d529f489537f179c05d50c2f3:timer', 'i:1747964598;', 1747964598);
 
 -- --------------------------------------------------------
 
@@ -48,8 +48,8 @@ INSERT INTO `cache` (`key`, `value`, `expiration`) VALUES
 --
 
 CREATE TABLE `cache_locks` (
-  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `owner` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `owner` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `expiration` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -61,11 +61,11 @@ CREATE TABLE `cache_locks` (
 
 CREATE TABLE `failed_jobs` (
   `id` bigint UNSIGNED NOT NULL,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uuid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -92,7 +92,13 @@ CREATE TABLE `item_orcamentos` (
 
 INSERT INTO `item_orcamentos` (`id`, `orcamento_id`, `produto_id`, `quantidade`, `preco_unitario`, `desconto`, `created_at`, `updated_at`) VALUES
 (1, 1, 1, 4, 4.95, 50.00, '2025-05-22 16:17:36', '2025-05-22 16:17:36'),
-(2, 1, 1, 3, 4.95, 0.00, '2025-05-22 16:23:11', '2025-05-22 16:23:11');
+(2, 1, 1, 3, 4.95, 0.00, '2025-05-22 16:23:11', '2025-05-22 16:23:11'),
+(3, 2, 1, 2, 4.95, 50.00, '2025-05-22 23:01:04', '2025-05-22 23:01:04'),
+(4, 2, 2, 6, 5.95, 33.33, '2025-05-23 01:14:18', '2025-05-23 01:14:18'),
+(5, 3, 1, 1, 4.95, 0.00, '2025-05-23 01:21:54', '2025-05-23 01:21:54'),
+(6, 3, 1, 2, 4.95, 50.00, '2025-05-23 01:22:09', '2025-05-23 01:22:09'),
+(10, 4, 3, 3, 6.75, 33.33, '2025-05-23 01:33:26', '2025-05-23 01:33:26'),
+(11, 2, 3, 3, 6.75, 33.33, '2025-05-23 01:39:57', '2025-05-23 01:39:57');
 
 -- --------------------------------------------------------
 
@@ -102,8 +108,8 @@ INSERT INTO `item_orcamentos` (`id`, `orcamento_id`, `produto_id`, `quantidade`,
 
 CREATE TABLE `jobs` (
   `id` bigint UNSIGNED NOT NULL,
-  `queue` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `attempts` tinyint UNSIGNED NOT NULL,
   `reserved_at` int UNSIGNED DEFAULT NULL,
   `available_at` int UNSIGNED NOT NULL,
@@ -117,13 +123,13 @@ CREATE TABLE `jobs` (
 --
 
 CREATE TABLE `job_batches` (
-  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `total_jobs` int NOT NULL,
   `pending_jobs` int NOT NULL,
   `failed_jobs` int NOT NULL,
-  `failed_job_ids` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `options` mediumtext COLLATE utf8mb4_unicode_ci,
+  `failed_job_ids` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `options` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `cancelled_at` int DEFAULT NULL,
   `created_at` int NOT NULL,
   `finished_at` int DEFAULT NULL
@@ -137,7 +143,7 @@ CREATE TABLE `job_batches` (
 
 CREATE TABLE `migrations` (
   `id` int UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -177,7 +183,9 @@ CREATE TABLE `ofertas` (
 --
 
 INSERT INTO `ofertas` (`id`, `produto_id`, `quantidade_levar`, `quantidade_pagar`, `data_validade`, `created_at`, `updated_at`) VALUES
-(1, 1, 2, 1, '2025-05-21', '2025-05-22 16:16:57', '2025-05-22 16:16:57');
+(1, 1, 2, 1, '2025-05-23', '2025-05-22 16:16:57', '2025-05-22 23:00:38'),
+(2, 2, 3, 2, '2025-05-31', '2025-05-23 01:13:44', '2025-05-23 01:13:44'),
+(3, 3, 3, 2, '2025-05-31', '2025-05-23 01:20:25', '2025-05-23 01:20:25');
 
 -- --------------------------------------------------------
 
@@ -189,7 +197,7 @@ CREATE TABLE `orcamentos` (
   `id` bigint UNSIGNED NOT NULL,
   `vendedor_id` bigint UNSIGNED NOT NULL,
   `cliente_id` bigint UNSIGNED NOT NULL,
-  `status` enum('rascunho','finalizado','cancelado') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'rascunho',
+  `status` enum('rascunho','finalizado','cancelado') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'rascunho',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -199,7 +207,10 @@ CREATE TABLE `orcamentos` (
 --
 
 INSERT INTO `orcamentos` (`id`, `vendedor_id`, `cliente_id`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, 2, 'finalizado', '2025-05-22 16:12:57', '2025-05-22 16:36:05');
+(1, 1, 2, 'finalizado', '2025-05-22 16:12:57', '2025-05-22 16:36:05'),
+(2, 1, 2, 'rascunho', '2025-05-22 23:00:50', '2025-05-22 23:00:50'),
+(3, 2, 3, 'finalizado', '2025-05-23 01:21:38', '2025-05-23 01:22:17'),
+(4, 2, 3, 'finalizado', '2025-05-23 01:24:34', '2025-05-23 01:33:36');
 
 -- --------------------------------------------------------
 
@@ -208,8 +219,8 @@ INSERT INTO `orcamentos` (`id`, `vendedor_id`, `cliente_id`, `status`, `created_
 --
 
 CREATE TABLE `password_reset_tokens` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -221,9 +232,9 @@ CREATE TABLE `password_reset_tokens` (
 
 CREATE TABLE `pessoas` (
   `id` bigint UNSIGNED NOT NULL,
-  `codigo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `codigo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nome` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `eh_cliente` tinyint(1) NOT NULL DEFAULT '0',
   `eh_vendedor` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
@@ -236,7 +247,8 @@ CREATE TABLE `pessoas` (
 
 INSERT INTO `pessoas` (`id`, `codigo`, `nome`, `email`, `eh_cliente`, `eh_vendedor`, `created_at`, `updated_at`) VALUES
 (1, '1001', 'Admin', 'admin@drogaria.com.br', 0, 1, '2025-05-22 15:09:05', '2025-05-22 15:09:05'),
-(2, '1002', 'Gustavo Sousa', NULL, 1, 0, '2025-05-22 15:57:30', '2025-05-22 15:57:30');
+(2, '1002', 'Gustavo Sousa', 'gustavo@drogaria.com.br', 1, 1, '2025-05-22 15:57:30', '2025-05-23 01:20:55'),
+(3, '1003', 'Andreia Cruz', NULL, 1, 0, '2025-05-23 01:19:43', '2025-05-23 01:19:43');
 
 -- --------------------------------------------------------
 
@@ -246,8 +258,8 @@ INSERT INTO `pessoas` (`id`, `codigo`, `nome`, `email`, `eh_cliente`, `eh_vended
 
 CREATE TABLE `produtos` (
   `id` bigint UNSIGNED NOT NULL,
-  `codigo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `descricao` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `codigo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descricao` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `preco` decimal(10,2) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -258,7 +270,9 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `codigo`, `descricao`, `preco`, `created_at`, `updated_at`) VALUES
-(1, '1001', 'DORFLEX', 4.95, '2025-05-22 16:16:32', '2025-05-22 16:16:32');
+(1, '1001', 'DORFLEX', 4.95, '2025-05-22 16:16:32', '2025-05-22 16:16:32'),
+(2, '1002', 'NOVALGINA', 5.95, '2025-05-23 01:13:16', '2025-05-23 01:13:16'),
+(3, '1003', 'DIPIRONA', 6.75, '2025-05-23 01:19:58', '2025-05-23 01:19:58');
 
 -- --------------------------------------------------------
 
@@ -267,11 +281,11 @@ INSERT INTO `produtos` (`id`, `codigo`, `descricao`, `preco`, `created_at`, `upd
 --
 
 CREATE TABLE `sessions` (
-  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` bigint UNSIGNED DEFAULT NULL,
-  `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `user_agent` text COLLATE utf8mb4_unicode_ci,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ip_address` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_agent` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `last_activity` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -280,7 +294,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('FiHLgAWUQdL7JZoPNjp7Uvm3KuwDCmlustpdeZYo', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiOThrQks4aVFTUXVrNmM1MDhsT1JjSlZYVk03aHBUYUdBTkNvYXRDTyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzU6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9wZXNzb2FzIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTtzOjE3OiJwYXNzd29yZF9oYXNoX3dlYiI7czo2MDoiJDJ5JDEyJERVNFg1WTRDSllCbUp0a3U4cEtzRC5rV3ZVbm9ZSjdsaHlvc1Q3enRncGYwVXI4T1A4SWNlIjtzOjg6ImZpbGFtZW50IjthOjA6e319', 1747932142);
+('8Scfdlq21SbHZ9u68S7AmWF4YjVIWOc9ZEV6c97f', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 OPR/118.0.0.0 (Edition std-2)', 'YToyOntzOjY6Il90b2tlbiI7czo0MDoiYVN2R0RrQzhMa0RUdXVGZjRYNVZ1aHlEQWdQdDduQnVNd0FRbFE5WCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1747963444),
+('D8YoOGfALdI58XxKVJTd44rb6RbwMcgwiQNIrImU', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 OPR/118.0.0.0 (Edition std-2)', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiTlJZOFpzM1lDM3hwdDRjb1dLMEYyUmY1aEltWVpGTGpKelpDZU9NRiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDI6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9wZXNzb2FzLzIvZWRpdCI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7czoxNzoicGFzc3dvcmRfaGFzaF93ZWIiO3M6NjA6IiQyeSQxMiREVTRYNVk0Q0pZQm1KdGt1OHBLc0Qua1d2VW5vWUo3bGh5b3NUN3p0Z3BmMFVyOE9QOEljZSI7fQ==', 1747965165);
 
 -- --------------------------------------------------------
 
@@ -290,15 +305,15 @@ INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, 
 
 CREATE TABLE `users` (
   `id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `pessoa_id` int NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `permission` enum('gestor','gerente','vendedor') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'vendedor'
+  `permission` enum('gestor','gerente','vendedor') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'vendedor'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -306,7 +321,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `pessoa_id`, `remember_token`, `created_at`, `updated_at`, `permission`) VALUES
-(1, 'Admin', 'admin@drogaria.com.br', '2025-05-22 15:09:06', '$2y$12$DU4X5Y4CJYBmJtku8pKsD.kWvUnoYJ7lhyosT7ztgpf0Ur8OP8Ice', 1, 'rYDB2oOLRfcN3ls668cH8WqS6I4UTP2rXMUhwjSED8ZziV7IPfrfafGSbwsl', '2025-05-22 15:09:06', '2025-05-22 15:09:06', 'gestor');
+(1, 'Admin', 'admin@drogaria.com.br', '2025-05-22 15:09:06', '$2y$12$DU4X5Y4CJYBmJtku8pKsD.kWvUnoYJ7lhyosT7ztgpf0Ur8OP8Ice', 1, 'RV6LMRjien755IWRHoguhCF82iFdbNf2qhYgKOtKRiZIWv7BkiAlrofMo8SP', '2025-05-22 15:09:06', '2025-05-22 15:09:06', 'gestor'),
+(2, 'Gustavo Sousa', 'gustavo@drogaria.com.br', NULL, '$2y$12$fqYgoVRfJPSowP581mpObuZCbsqhhLq1Z7EVzypBlHjGQ2FVxElIW', 2, NULL, '2025-05-23 01:20:55', '2025-05-23 01:20:55', 'vendedor');
 
 --
 -- Índices para tabelas despejadas
@@ -422,7 +438,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT de tabela `item_orcamentos`
 --
 ALTER TABLE `item_orcamentos`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de tabela `jobs`
@@ -440,31 +456,31 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT de tabela `ofertas`
 --
 ALTER TABLE `ofertas`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `orcamentos`
 --
 ALTER TABLE `orcamentos`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `pessoas`
 --
 ALTER TABLE `pessoas`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restrições para tabelas despejadas
@@ -474,7 +490,7 @@ ALTER TABLE `users`
 -- Restrições para tabelas `item_orcamentos`
 --
 ALTER TABLE `item_orcamentos`
-  ADD CONSTRAINT `item_orcamentos_orcamento_id_foreign` FOREIGN KEY (`orcamento_id`) REFERENCES `pessoas` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `item_orcamentos_orcamento_id_foreign` FOREIGN KEY (`orcamento_id`) REFERENCES `orcamentos` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `item_orcamentos_produto_id_foreign` FOREIGN KEY (`produto_id`) REFERENCES `pessoas` (`id`) ON DELETE RESTRICT;
 
 --
